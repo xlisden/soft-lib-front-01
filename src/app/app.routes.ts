467be9ea11b94370eadb58b7087892cb1db8fc9f
xlisden <dayenira.delgado@gmail.com>
2025-10-29 19:02:43 -0500

    init config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LibroListComponent } from './components/libro-list/libro-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/libros',
+    pathMatch: 'full',
+  },
+  {
+    path: 'libros',
+    component: LibroListComponent,
+  },
+];
